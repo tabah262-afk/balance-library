76fa3c3f4abba5733e1dee54c7a54e0aab1ce99c
Tabah Pangestu <tabah262@smk.belajar.id>
2026-07-12 15:14:48 +0700

    Restore database connection

---
+++ database/database.py
@@ -0,0 +1,51 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+# ==========================
+# Konfigurasi Google Sheets
+# ==========================
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_file(
+    "database/credentials.json",
+    scopes=SCOPES
+)
+
+client = gspread.authorize(creds)
+
+# ==========================
+# Buka Spreadsheet
+# ==========================
+
+spreadsheet = client.open("Balance Library Database")
+
+# ==========================
+# Ambil Sheet
+# ==========================
+
+users_sheet = spreadsheet.worksheet("Users")
+books_sheet = spreadsheet.worksheet("Books")
+categories_sheet = spreadsheet.worksheet("Categories")
+mybooks_sheet = spreadsheet.worksheet("My Books")
+
+# ==========================
+# Functions
+# ==========================
+
+def get_users():
+    return users_sheet.get_all_records()
+
+def get_books():
+    return books_sheet.get_all_records()
+
+def get_categories():
+    return categories_sheet.get_all_records()
+
+def get_mybooks():
+    return mybooks_sheet.get_all_records()
+
+print(get_books())
